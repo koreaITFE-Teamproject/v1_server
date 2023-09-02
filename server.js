0491d0c04d3e2396b00f1dcf7a400227de1389ad
king2992 <41357320+king2992@users.noreply.github.com>
2023-09-02 17:22:49 +0900

    server init

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+
+const mysql = require("mysql2");
+
+// MySQL 접속 주소를 기반으로 연결 정보 설정
+const connection = mysql.createConnection({
+  host: "139.150.69.126", // MySQL 호스트 주소
+  user: "root", // MySQL 사용자 이름
+  password: "Koreait1234!", // MySQL 비밀번호
+  database: "koreait", // 연결할 데이터베이스 이름
+  port: 3306, // MySQL 포트 번호
+  ssl: {
+    rejectUnauthorized: false, // SSL 설정 (주의: 개발 환경에서만 사용)
+    require: true,
+  },
+});
+
+// MySQL 연결
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+  } else {
+    console.log("Connected to MySQL database.");
+  }
+});
+
+app.get("/", (req, res) => {
+  // 데이터베이스 조회 쿼리
+  const query = "SELECT * FROM temp"; // 본인의 테이블 이름으로 수정
+  connection.query(query, (queryErr, results) => {
+    if (queryErr) {
+      console.error("Error executing query:", queryErr);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    console.log("Database results:", results);
+
+    res.send("Hello, Node.js! Check the console for database results.");
+  });
+});
+
+// 서버 종료 시 MySQL 연결 종료
+process.on("SIGINT", () => {
+  connection.end((endErr) => {
+    if (endErr) {
+      console.error("Error ending connection:", endErr);
+    } else {
+      console.log("Connection to MySQL database closed.");
+    }
+    process.exit(); // 서버 종료
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
